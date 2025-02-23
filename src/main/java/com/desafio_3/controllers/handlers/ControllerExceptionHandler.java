6f3c0e3ade42563c57c125bdeb2650e54c5509a0
Victor Da Costa Vasco <victordcvasco@gmail.com>
2025-02-23 16:27:40 -0300

    Exceptions

---
+++ src/main/java/com/desafio_3/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.desafio_3.controllers.handlers;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.desafio_3.dto.CustomError;
+import com.desafio_3.services.exceptions.DatabaseException;
+import com.desafio_3.services.exceptions.ResourceNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<CustomError> recourceNotfound(ResourceNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
--- src/main/java/com/desafio_3/services/ClientService.java
+++ src/main/java/com/desafio_3/services/ClientService.java
@@ -1,15 +1,21 @@
 package com.desafio_3.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.desafio_3.dto.ClientDTO;
 import com.desafio_3.entities.Client;
 import com.desafio_3.mappers.ClientMapper;
 import com.desafio_3.repositories.ClientRepository;
+import com.desafio_3.services.exceptions.DatabaseException;
+import com.desafio_3.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ClientService {
@@ -19,7 +25,7 @@ public class ClientService {
 	
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
-		Client client = repository.findById(id).get();
+		Client client = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
 		return ClientMapper.INSTANCE.toDTO(client);
 	}
 	
@@ -39,15 +45,27 @@ public class ClientService {
 	
 	@Transactional
 	public ClientDTO update(Long id, ClientDTO dto) {
-		Client client = repository.getReferenceById(id);
-		ClientMapper.INSTANCE.updateFromDTO(dto, client);
-		Client updatedClient = repository.save(client);
-		return ClientMapper.INSTANCE.toDTO(updatedClient);
+		try {
+			Client client = repository.getReferenceById(id);
+			ClientMapper.INSTANCE.updateFromDTO(dto, client);
+			Client updatedClient = repository.save(client);
+			return ClientMapper.INSTANCE.toDTO(updatedClient);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Recurso Não encontrado");
+		}
 	}
 	
-	@Transactional
+	@Transactional(propagation = Propagation.SUPPORTS)
 	public void delete(long id) {
-		repository.deleteById(id);
+		if(!repository.existsById(id)) {
+			throw new ResourceNotFoundException("Recurso não encontrado");
+		}
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Falha de integridade referencial");
+		}
+		
 	}
 	
 }
